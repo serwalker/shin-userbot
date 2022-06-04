307bd0c1a4e2648d0fe731f2b653029d7546e791
serwalker <105328357+serwalker@users.noreply.github.com>
2022-06-05 04:18:09 +0800

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -92,7 +92,7 @@ PM_LIMIT = int(os.environ.get("PM_LIMIT", 6))
 # Custom Pmpermit pic
 PMPERMIT_PIC = (
     os.environ.get("PMPERMIT_PIC", None)
-    or "https://telegra.ph/file/b8fc0d231d47ba1c2cbc0.jpg"
+    or "https://telegra.ph/file/9ffcecae609a1aae73971.jpg"
 )
 
 # Bleep Blop, this is a bot ;)
@@ -220,11 +220,11 @@ S_PACK_NAME = os.environ.get("S_PACK_NAME", None)
 
 # Default .alive Logo
 ALIVE_LOGO = (os.environ.get("ALIVE_LOGO")
-              or "https://telegra.ph/file/b8fc0d231d47ba1c2cbc0.jpg")
+              or "https://telegra.ph/file/9ffcecae609a1aae73971.jpg")
 
 # Default .helpme Logo
 INLINE_PIC = (os.environ.get("INLINE_PIC")
-              or "https://telegra.ph/file/b8fc0d231d47ba1c2cbc0.jpg")
+              or "https://telegra.ph/file/9ffcecae609a1aae73971.jpg")
 
 # Default emoji help
 EMOJI_HELP = os.environ.get("EMOJI_HELP") or "✗"
